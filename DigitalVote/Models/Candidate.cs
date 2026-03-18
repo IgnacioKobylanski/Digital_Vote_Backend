1d8b2b76b19258bd1f437d66bbd34e439c826c3a
IgnacioKobylanski <124396067+IgnacioKobylanski@users.noreply.github.com>
2026-03-18 18:53:45 -0300

    Candidate model now have two imgs

--- DigitalVote/Models/Candidate.cs
+++ DigitalVote/Models/Candidate.cs
@@ -5,5 +5,7 @@
         public string Name { get; set; } = string.Empty;
         public string Party { get; set; } = string.Empty;
         public string Position { get; set; } = string.Empty;
+        public string? CandidateImageUrl { get; set; }
+        public string? PartyLogoUrl { get; set; }
     }
 }
