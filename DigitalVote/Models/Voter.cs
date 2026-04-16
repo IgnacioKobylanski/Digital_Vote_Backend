b87ef0a46c5f147b3ed20b9e97fba0c3ae583851
IgnacioKobylanski <124396067+IgnacioKobylanski@users.noreply.github.com>
2026-04-16 20:13:16 -0300

    minor change in Voter.cs model

--- DigitalVote/Models/Voter.cs
+++ DigitalVote/Models/Voter.cs
@@ -6,7 +6,7 @@
         public string Dni { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string FullName => $"{LastName}, {FirstName}";
+        public string FullName => $"{LastName}  {FirstName}";
 
         public bool HasVoted { get; set; } = false;
         public DateTime? VoteDate { get; set; }
